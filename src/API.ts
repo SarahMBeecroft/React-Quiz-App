6d837fa61b240a1bc6547b2218ce83db2aa888f9
Sarah Beecroft <sarahbeecroft@Sarahs-MacBook-Pro.local>
2020-07-18 11:33:22 -0700

    Got API call working to fetch quiz questions

---
+++ src/API.ts
@@ -0,0 +1,15 @@
+export enum Difficulty {
+  EASY = 'easy',
+  MEDIUM = 'medium',
+  HARD = 'hard',
+}
+
+export const fetchQuizQuestions = async (
+  amount: number,
+  difficulty: Difficulty
+) => {
+  const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
+  // Two awaits, one for fetch itself and one to convert data to json
+  const data = await (await fetch(endpoint)).json();
+  console.log(data);
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,52 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { fetchQuizQuestions } from './API';
+// Components
+// import QuestionCard from './components/QuestionCard';
+// Types
+import { Difficulty } from './API';
 
-function App() {
-  return <div className='App'>Quiz</div>;
-}
+const TOTAL_QUESTIONS = 10;
+
+const App: React.FC = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY));
+
+  // Function to start quiz
+  const startQuiz = async () => {};
+
+  // Function to check answer
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
+
+  // Function to move to next question
+  const nextQuestion = () => {};
+
+  return (
+    <div className='App'>
+      <h1>React Quiz</h1>
+      <button className='start' onClick={startQuiz}>
+        Start
+      </button>
+      <p className='score'>Score:</p>
+      <p>Loading question...</p>
+      {/* <QuestionCard
+        questionNm={number + 1}
+        totalQuestions={TOTAL_QUESTIONS}
+        question={questions[number].question}
+        answers={questions[number].answers}
+        userAnswer={userAnswers ? userAnswers[number] : undefined}
+        callback={checkAnswer}
+      /> */}
+      <button className='next' onClick={nextQuestion}>
+        Next question
+      </button>
+    </div>
+  );
+};
 
 export default App;
